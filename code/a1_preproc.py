682ff702d258a233d558db621b7a0edc57372c41
liwuqi95 <wuqi.li@mail.utoronto.ca>
2019-02-11 10:13:31 -0500

    finalize part1

--- code/a1_preproc.py
+++ code/a1_preproc.py
@@ -9,6 +9,7 @@ import spacy
 
 # indir = '/u/cs401/A1/data/';
 indir = '../data/'
+# indir = '/u/cs401/A1/wordlists/';
 listdir = '../wordlists/'
 
 punctuation_re = r'[' + string.punctuation + ']*|\w*'
@@ -34,52 +35,70 @@ def preproc1(comment, steps=range(1, 11)):
     Returns:
         modComm : string, the modified comment 
     '''
+    #set the modComm
     modComm = comment
     if 1 in steps:
-        modComm = comment.replace('\n', ' ')
+        # remove new line
+        modComm = modComm.replace('\n', ' ')
     if 2 in steps:
+        # use html to remove html special characters
         modComm = html.unescape(modComm)
     if 3 in steps:
+        # remove http or www url ending with space
         modComm = re.sub("(http|www).*\s", " ", modComm)
     if 4 in steps:
+        # insert space aside the group of punctuations
         modComm = re.sub(r"([" + string.punctuation + "]+)", r" \1 ", modComm)
+        # set Apostrophes back normal
         modComm = modComm.replace(" ...", "...")
+        # set abbreviations back normal
         for i in range(len(abbrev_list_space_1000292033)):
             modComm = modComm.replace(abbrev_list_space_1000292033[i], abbrev_list_1000292033[i])
     if 5 in steps:
+        # consider two cases.
+        # 1. the punctuations are already splited with a space
+        # 2. the punctuations are in normal condition
         for i in range(len(clitic_list_1000292033)):
             modComm = modComm.replace(clitic_list_space_1000292033[i], clitic_list_1000292033[i])
             modComm = modComm.replace(clitic_list_1000292033[i], ' ' + clitic_list_1000292033[i] + ' ')
     if 6 in steps:
+        # remove extra spaces
         modComm = ' '.join(modComm.split())
         utt = nlp(modComm)
         token_list = []
 
+        # set token in required format
         for token in utt:
             token_list.append(token.text + '/' + token.tag_)
         modComm = ' '.join(token_list)
     if 7 in steps:
+        # remove stop words
         for stop_word in stop_list_1000292033:
             modComm = re.sub("\s" + stop_word + "/\w*\s", " ", modComm)
     if 8 in steps:
+        # replace token text with lemma
         for token in utt:
             if token.text != token.lemma_:
                 if not (token.lemma_.startswith('-') and not token.text.startswith('-')):
                     modComm = modComm.replace(token.text, token.lemma_)
     if 9 in steps:
+        # insert new line after group of !?.
         modComm = re.sub(r"([!?.]/[.]\s)(\w)", r"\1 \n \2", modComm)
     if 10 in steps:
         token_list = modComm.split(" ")
-
         for i in range(len(token_list)):
+            # check if the spacy tag applied
             token_info = token_list[i].rsplit('/', 1)
             if len(token_info) > 1:
+                # case one, after spacy
                 token_list[i] = token_info[0].lower() + '/' + token_info[1]
             else:
+                # case two, raw data
                 token_list[i] = token_list[i].lower()
 
         modComm = " ".join(token_list)
 
+    # remove extra space
     modComm = re.sub(r" +", " ", modComm)
     return modComm
 
@@ -93,22 +112,16 @@ def main(args):
             data = json.load(open(fullFile))
 
             count = args.max
+            # set index with student number
             i = args.ID[0] % len(data)
 
             while count > 0:
                 j = json.loads(data[i])
                 line = {"id": j["id"], "cat": file, 'body': preproc1(j["body"])}
                 allOutput.append(line)
+                # circular index
                 i = i + 1 if i < len(data) - 1 else 0
                 count = count - 1
-
-            # TODO: select appropriate args.max lines
-            # TODO: read those lines with something like `j = json.loads(line)`
-            # TODO: choose to retain fields from those lines that are relevant to you
-            # TODO: add a field to each selected line called 'cat' with the value of 'file' (e.g., 'Alt', 'Right', ...) 
-            # TODO: process the body field (j['body']) with preproc1(...) using default for `steps` argument
-            # TODO: replace the 'body' field with the processed text
-            # TODO: append the result to 'allOutput'
 
     fout = open(args.output, 'w')
     fout.write(json.dumps(allOutput))
